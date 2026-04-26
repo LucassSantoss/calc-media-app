0a5c0a7f6231bb06b1795dc6a545fdfdee56980f
LucassSantoss <lucascross.pereirasantos@gmail.com>
2026-04-25 23:03:44 -0300

    feat: remove template code and create calcMedia widget

---
+++ lib/components/calcMediaForm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalcMediaForm extends StatefulWidget {
+  const CalcMediaForm({super.key});
+
+  @override
+  State<CalcMediaForm> createState() => _CalcMediaFormState();
+}
+
+class _CalcMediaFormState extends State<CalcMediaForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
